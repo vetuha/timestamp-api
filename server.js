f7f689e972573ffafaa36a1faa09e085dac83632
Vitalii Moroz <v.vetuha@gmail.com>
2016-07-28 15:02:30 +0000

    work is finished

--- server.js
+++ server.js
@@ -1,11 +1,51 @@
 var express = require('express');
+var strftime = require('strftime');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static(__dirname + '/views'));
+
+app.all("*", function(request, response, next) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  next();
+});
+
+app.get("/", function(request, response) {
+    response.sendFile("/index.html");
+});
+
+app.get("/:qtime", function(request, response) {
+    var time = parseParams(decodeURIComponent(request.params.qtime))
+    
+    response.end(JSON.stringify(getTS(time)));
+});
+
+app.get("*", function(request, response) {
+  response.end("404!");
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('timestamp-api app listening on port 8080!');
 })
+
+function getTS (time) {
+  return {
+    unix: time ? time.getTime()/1000 : null,
+    natural: time ? strftime('%B %d, %Y', time) : null
+  }
+};
+
+function parseParams (dateParam) {
+  var pTime;
+  if(/^\d+$/.test(dateParam)){
+      pTime = new Date(+dateParam*1000);
+  }
+  else{
+      pTime = new Date(dateParam);
+  }
+  
+  return pTime != "Invalid Date" ? pTime : null;
+};
+
+
+//decodeURIComponent();
